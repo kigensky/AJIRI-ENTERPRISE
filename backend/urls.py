67d25f7d232109883f2b02673d15e539fae2cab9
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-17 09:31:29 +0300

    update

--- backend/urls.py
+++ backend/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('auth/', include('rest_auth.urls')),
     path('auth/signup/', include('rest_auth.registration.urls')),
     path('auth/refresh-token/', refresh_jwt_token),
-    path('api/employee/',views.EmployeeList.as_view(),name='employee'),
-    path('employee/update/<name>/',views.EmployeeList.as_view(),name='update_employee'),
-    path('employee/delete/<int:pk>/',views.EmployeeList.as_view(),name='delete_employee'),
+    #path('api/employee/',views.EmployeeList.as_view(),name='employee'),
+    #path('employee/update/<name>/',views.EmployeeList.as_view(),name='update_employee'),
+    #path('employee/delete/<int:pk>/',views.EmployeeList.as_view(),name='delete_employee'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
