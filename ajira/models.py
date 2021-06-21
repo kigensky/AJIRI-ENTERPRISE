a9d3a7aca487ab70d2f2bdc2d3b6f53494f0dbd9
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-21 23:27:54 +0300

    update

--- ajira/models.py
+++ ajira/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
