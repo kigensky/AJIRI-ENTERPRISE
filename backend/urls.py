634f4f18d584ba1289f107f603bb81cf86e1348d
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-21 18:45:47 +0300

    update

--- backend/urls.py
+++ backend/urls.py
@@ -25,7 +25,7 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls'))
+    path('api/', include('ajira.urls'))
     
     
 ]
