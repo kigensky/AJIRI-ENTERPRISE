78cd947fd65d3d50c90f551bbf9ca0ea5d4a72f4
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-23 12:14:05 +0300

    update

--- ajira/urls.py
+++ ajira/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from .views import RegisterView, LoginView, UserView, LogoutView
+from django.urls import include, path
+from rest_framework import routers
+from .import views
 
 urlpatterns = [
     
@@ -8,4 +11,13 @@ urlpatterns = [
     path('login', LoginView.as_view()),
     path('user', UserView.as_view()),
     path('logout', LogoutView.as_view()),
+]
+router = routers.DefaultRouter()
+router.register(r'profile',views.ProfileViewSet)
+router.register(r'employee_salary',views.EmployeeSalaryViewset)
+router.register(r'employee',views.EmployeeViewset)
+router.register(r'leave',views.LeaveViewset)
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),  
 ]
--- ajira/views.py
+++ ajira/views.py
@@ -8,6 +8,17 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
 import jwt, datetime
+from django.contrib.auth.models import User
+from django.http import response
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.views import APIView
+from .models import Employee, EmployeeSalary,Profile,Leave
+from rest_framework.response import Response
+from django.http import Http404
+from .serializers import *
+from rest_framework import status
+from .serializers import EmployeeSerializer, UserSerializer,EmployeeSalarySerializer,ProfileSerializer,LeaveSerializer
 
 
 # Create your views here.
@@ -75,3 +86,39 @@ class LogoutView(APIView):
         }
 
         return response
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows profile to be viewed or edited.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+class EmployeeSalaryViewset(viewsets.ModelViewSet):
+    """
+    API endpoint that allows salary to be viewed or edited.
+    """
+    queryset = EmployeeSalary.objects.all()
+    serializer_class = EmployeeSalarySerializer
+    permission_classes = [permissions.IsAuthenticated]
+class EmployeeViewset(viewsets.ModelViewSet):
+    """
+    API endpoint that lists employees.
+    """
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+class LeaveViewset(viewsets.ModelViewSet):
+    """
+    API endpoint shows employees leave dates.
+    """
+    queryset = Leave.objects.all()
+    serializer_class = LeaveSerializer
+    permission_classes = [permissions.IsAuthenticated]
