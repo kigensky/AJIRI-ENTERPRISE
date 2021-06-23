4156736b828610091809621b374a2dfb6e2ef182
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-23 12:23:14 +0300

    update

--- ajira/urls.py
+++ ajira/urls.py
@@ -5,13 +5,7 @@ from django.urls import include, path
 from rest_framework import routers
 from .import views
 
-urlpatterns = [
-    
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
-]
+
 router = routers.DefaultRouter()
 router.register(r'profile',views.ProfileViewSet)
 router.register(r'employee_salary',views.EmployeeSalaryViewset)
@@ -19,5 +13,12 @@ router.register(r'employee',views.EmployeeViewset)
 router.register(r'leave',views.LeaveViewset)
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls')),  
+    path('api-auth/', include('rest_framework.urls')),
+    
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
+   
 ]
+
