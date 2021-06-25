b4cd7703ee0dd87966cd415f5ed0dcc17661e518
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-25 17:25:19 +0300

    update

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,6 +96,9 @@ DATABASES = {
         'PORT': '5432',    
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
